7a55d596b260fa2a600ee6d0617b5b5ad2646db0
ODBreno <bre.oliveira.dias@gmail.com>
2023-11-13 16:41:52 -0300

    Update criar_banco.sql

--- SQL/criar_banco.sql
+++ SQL/criar_banco.sql
@@ -1,4 +1,25 @@
--- TODO: users, indices --
+-- TODO:indices --
+
+CREATE ROLE usuario;
+CREATE ROLE dev;
+CREATE ROLE dba;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO usuario;
+GRANT SELECT, INSERT, DELETE, UPDATE ON ALL TABLES IN SCHEMA public TO dev;
+GRANT ALL ON DATABASE "trabalho_bd2" TO dba;
+
+CREATE USER deputadosDBA WITH PASSWORD 'deputadosDBA';
+CREATE USER deputadosDEV WITH PASSWORD 'deputadosDEV';
+CREATE USER deputadosUSER WITH PASSWORD 'deputadosUSER';
+
+GRANT usuario TO deputadosUSER;
+GRANT dev TO deputadosDEV;
+GRANT dba TO deputadosDBA;
+
+GRANT USAGE ON SCHEMA public TO martinsUSER;
+GRANT USAGE ON SCHEMA public TO martinsDEV;
+GRANT USAGE ON SCHEMA public TO martinsDBA;
+
 
 -- TABELAS --
 CREATE TABLE legislatura (
@@ -71,15 +92,36 @@ CREATE TABLE evento_deputado(
     FOREIGN KEY (id_deputado) REFERENCES deputados(id)
 )
 
-
+-- CONSULTAS --
 SELECT * FROM legislatura
 SELECT * FROM orgaos
 SELECT * FROM despesas
 SELECT * FROM evento
+SELECT * FROM deputados
 SELECT * FROM evento_deputado 
+SELECT * FROM evento_orgao
+SELECT * FROM deputado_orgao
 SELECT * FROM deputados join legislatura on deputados.idlegislatura = legislatura.id
 
 DELETE FROM deputados
 DELETE FROM despesas
+
+CREATE OR REPLACE FUNCTION count_rows(tablename text) 
+    RETURNS integer AS
+    	$$
+    	DECLARE
+    		result integer;
+    	BEGIN
+    		EXECUTE 'SELECT count(*) FROM ' || 
+                tablename INTO result;
+    		RETURN result;
+    	END;
+        $$ language plpgsql;
+
+SELECT table_name, count_rows(table_name) 
+FROM information_schema.tables 
+WHERE table_schema NOT IN 
+	('pg_catalog', 'information_schema') 
+ORDER by count_rows(table_name) DESC
 
 
